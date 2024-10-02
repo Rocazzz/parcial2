77aeabd778eca12b52fc772f54a747a516336a0b
Rocazzz <camiloakarck@gmail.com>
2024-10-02 18:47:16 -0500

    Finalización de Skills, con SkillCard, SkillInfoCard para un mejor orden en los componentes de Skill, falta arreglar un error en la imagen Soft skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
-import './Skills.css'
+import React, { useState } from "react";
+import "./Skills.css";
+import SkillCard from "./SkillCard/SkillCard";
+import SkillsInfoCard from "./SkillsInfoCard/SkillsInfoCard";
+import { SKILLS } from "../../utils/data";
 
-const skills = () => {
+const Skills = () => {
+  const [selectedSkill, setSelectedSkill] = useState(SKILLS[0]);
+
+  const handleSelectSkill = (data) => {
+    setSelectedSkill(data);
+  };
+
   return (
-    <div>skills</div>
-  )
-}
+    <section id="skills" className="skills-container">
+      <h5>Hard and Soft Skills</h5>
 
-export default skills
+      <div className="skills-content">
+        <div className="skills">
+          {SKILLS.map((item) => (
+            <SkillCard
+              key={item.title}
+              iconUrl={item.icon}
+              title={item.title}
+              isActive={selectedSkill.title === item.title}
+              onClick={() => {
+                handleSelectSkill(item);
+              }}
+            />
+          ))}
+        </div>
+
+        <div className="skills-info">
+          <SkillsInfoCard
+            heading={selectedSkill.title}
+            skills={selectedSkill.skills}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
---
+++ src/utils/data.js
@@ -0,0 +1,26 @@
+export const SKILLS = [
+    {
+      title: "Hard skills",
+      icon: "src/assets/images/hard-skills.png",
+      skills: [
+        { skill: "HTML5", percentage: "80%" },
+        { skill: "CSS", percentage: "90%" },
+        { skill: "JavaScript", percentage: "75%" },
+        { skill: "React.js", percentage: "80%" },
+        { skill: "Figma", percentage: "70%" },
+        { skill: "Node.js", percentage: "60%" },
+      ],
+    },
+    {
+      title: "Soft skills",
+      icon: ".src/assets/images/hard-skills.png",
+      skills: [
+        { skill: "Creativity", percentage: "90%" },
+        { skill: "Problem solving", percentage: "70%" },
+        { skill: "Teamwork", percentage: "80%" },
+        { skill: "Effective communication", percentage: "90%" },
+        { skill: "Adaptability", percentage: "85%" },
+        { skill: "Initiative", percentage: "75%" },
+      ],
+    },
+  ];
