687b5ca042c8d0f47249a59161211b9b08ec72dd
Rocazzz <camiloakarck@gmail.com>
2024-10-02 10:11:43 -0500

    Navbar implementación jsx y css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
-import './Navbar.css'
+import React from 'react';
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+    <nav className="nav-wrapper">
+      <div className="nav-content">
+        <h1 className='logo'>My Portfolio</h1>
+        <ul>
+          <li>
+            <a className="menu-item">Home</a>
+          </li>
+          <li>
+            <a className="menu-item">Skills</a>
+          </li>
+          <li>
+            <a className="menu-item">Proyects</a>
+          </li>
+          <li>
+            <a className="menu-item">Contact Me</a>
+          </li>
+
+          <button className="contact-btn" onClick={() => {}}>
+            Hire Me
+          </button>
+        </ul>
+      </div>
+
+    </nav>
+      
+      </>
   )
 }
 
